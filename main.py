303bb3429729a54bdadee1ab0fddbac18a5195c3
FarisRaza123 <farisraza2023@u.northwestern.edu>
2021-02-17 22:29:56 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template,request
+from flask_restful import Api, Resource, fields, marshal_with
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+todos = {}
+
+
+@app.route('/notes', methods=['GET', 'POST', 'DELETE'])
+def getURL():
+    if request.method == 'POST':
+        id = request.form['id']
+        data = request.form['data']
+        print(id, data)
+        todos[id] = data
+        return {id: data}
+
+    if request.method == 'GET':
+        return todos
+
+    if request.method == 'DELETE':
+        id = request.form['id']
+        del todos[id]
+        return todos
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
